f38e3ff2935868fbbb9dced7610a06cbd21be5c0
Alejandro Meneses <Alejandro_Meneses@AlejandrosMBP2.attlocal.net>
2016-06-02 15:56:03 -0400

    feature section, pricing section, js for responsive

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,12 +1,35 @@
 $(document).ready(function() {
+
+    //Collapses Menu Items to Mobile Nav .
+
     $('.button-collapse').sideNav();
+
+    // Scroll function if button on header is clicked.
+
     $("button").click(function() {
         $('html,body').animate({
                 scrollTop: $("#section-3").offset().top
             },
             'slow');
     });
-    $('.card').hover(function() {
+    // Links on Nav smooth scroll down to respective sections with a smooth scroll.
+
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    // Gives cards the appearnce of floating off the page on hover.
+
+    $('.hoverable').hover(function() {
             $(this).removeClass('z-depth-0').addClass('z-depth-1');
         },
         function() {
@@ -14,6 +37,9 @@ $(document).ready(function() {
         }
 
     )
+
+    //Changes size of button depending on the size of the screen.
+
     var eventFired = 0;
 
     if ($(window).width() > 1400) {
@@ -32,5 +58,15 @@ $(document).ready(function() {
                 $('.sign-up-btn').removeClass('btn-large');
             }
         }
+    });
+
+    // Initialization of feature section tabs
+
+    $('ul.tabs').tabs();
+
+    // Opens intercom chat bubble when they click the "live chat support" card.
+
+    $('#live_chat_link').click(function() {
+        $('#intercom-launcher').trigger('click');
     });
 });
